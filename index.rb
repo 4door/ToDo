79e8d064f4059d852e7ff647ebf6c76bcd63d13f
4door <shiorishinohe@gmail.com>
2021-01-06 19:13:29 +0900

    update:riutes&dbsetting

--- index.rb
+++ index.rb
@@ -1,2 +1,10 @@
-puts "Hello, world!"
-puts 1 + 2
+require 'pg'
+
+connect = PG::connect(host: "localhost", user: "postgres", password: "postgres", dbname: "postgres", port: "5432") 
+results = connect.exec("SELECT city AS hoge FROM weather")
+
+results.each{|result|
+  p result["hoge"]
+}
+
+connect.finish
