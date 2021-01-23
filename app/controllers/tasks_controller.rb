1f156ddae22f63c91b884c6e31e7a7c9e79756f6
4door <shiorishinohe@gmail.com>
2021-01-23 19:38:43 +0900

    暫定

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,14 +5,17 @@ class TasksController < ApplicationController
     @status = ['新規', '対応中', '完了']  
   end
   def create
-    task = Task.new
-    task.task         = params[:task]
-    task.state        = params[:state]
-    task.limit_date   = params[:limit_date]
-    task.description  = params[:description]
-    task.user_id      = current_user.id
-    task.save
-    redirect_to '/tasks', notice: 'タスクを作成しました。'
+    @task = current_user.tasks.new task_params
+    @task.save!
+    redirect_to @task, notice: 'タスクを作成しました。'
+    # task = Task.new
+    # task.task         = params[:task]
+    # task.state        = params[:state]
+    # task.limit_date   = params[:limit_date]
+    # task.description  = params[:description]
+    # task.user_id      = current_user.id
+    # task.save
+    # redirect_to '/tasks', notice: 'タスクを作成しました。'
   end
   def show
     id      = params[:id]
@@ -33,5 +36,12 @@ class TasksController < ApplicationController
     task       = Task.find(params[:id])
     task.destroy
     redirect_to '/tasks', notice: 'タスクを削除しました。'
+  end
+  private
+  def target_task task_id
+    current_user.tasks.where(id: task_id).take
+  end
+  def task_params
+    params.require(:task).permit(:task, :limit_date, :state, :description)
   end
 end
