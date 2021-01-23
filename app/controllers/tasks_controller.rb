08a286bf3c927a5ac5c6d24677c9865fbf5bccbb
4door <shiorishinohe@gmail.com>
2021-01-23 16:33:19 +0900

    update

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,10 @@
 class TasksController < ApplicationController
   before_action :authenticate_user!
   def index
-    @tasks  = current_user.tasks
+    @tasks  = current_user.tasks.order(state: "DESC").order(limit_date: "ASC")
     @status = ['新規', '対応中', '完了']  
   end
-  def store
+  def create
     task = Task.new
     task.task         = params[:task]
     task.state        = params[:state]
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  get    'tasks'     => 'tasks#index'
-  post   'tasks'     => 'tasks#store'
-  get    'tasks/:id' => 'tasks#show'
-  put    'tasks/:id' => 'tasks#update'
-  delete 'tasks/:id' => 'tasks#destroy'
+  resources :tasks
+  # get    'tasks'     => 'tasks#index'
+  # post   'tasks'     => 'tasks#store'
+  # get    'tasks/:id' => 'tasks#show'
+  # put    'tasks/:id' => 'tasks#update'
+  # delete 'tasks/:id' => 'tasks#destroy'
 end
